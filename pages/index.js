941727a9d2f2fc7cf47e66b2a2022793699f6557
Trevor Lee <tlee297@my.bcit.ca>
2023-01-13 22:55:52 -0800

    can open evs and fs

--- pages/index.js
+++ pages/index.js
@@ -11,16 +11,16 @@ export default function Home() {
 
   async function getCards(arr) {
     await Promise.all([
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[0]}`),
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[1]}`),
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[2]}`),
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[3]}`),
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[4]}`),
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[5]}`),
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[6]}`),
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[7]}`),
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[8]}`),
-      fetch(`https://api.pokemontcg.io/v2/cards/swsh7-${arr[9]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[0]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[1]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[2]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[3]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[4]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[5]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[6]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[7]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[8]}`),
+      fetch(`https://api.pokemontcg.io/v2/cards/${expansion.id}-${arr[9]}`),
     ])
       .then(function (responses) {
         // Get a JSON object from each of the responses
@@ -45,27 +45,43 @@ export default function Home() {
 
   function getNumbers() {
     setIsLoaded(false)
-    let arr = [];
-    // Get 10 unique numbers in range 0?-220
+    let arr = []
+    console.log(expansion.total)
+    let numberOfCards = expansion.total
+    // Get 10 unique numbers in range 1 to n
     while (arr.length < 10) {
-      let r = Math.floor(Math.random() * 220) + 1;
-      if (arr.indexOf(r) === -1) arr.push(r);
+      let r = Math.floor(Math.random() * numberOfCards) + 1
+      if (arr.indexOf(r) === -1) arr.push(r)
     }
     console.log(arr);
     getCards(arr);
+  }
+
+  function getSet(expansionId) {
+    fetch(`https://api.pokemontcg.io/v2/sets/${expansionId}`)
+      .then((responses) => {
+        return responses.json()
+      })
+      .then((data) => {
+        setExpansion(data.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
     <div className="App">
       {expansion === '' ? <>
         <h1>Pick an expansion</h1>
-        <button onClick={() => { setExpansion('cel') }}>Celebrations</button>
-        <button onClick={() => { setExpansion('evs') }}>Evolving Skies</button>
+        <button onClick={() => { getSet('swsh8') }}>Fusion Strike</button>
+        <button onClick={() => { getSet('swsh7') }}>Evolving Skies</button>
       </> : <></>}
 
       {expansion !== '' ? <>
         <button onClick={() => { setExpansion('') }}>Back</button>
-        <h1>{expansion}</h1>
+        <h1>{expansion.name}</h1>
+        <h1>{expansion.total}</h1>
         <button onClick={getNumbers}>Get cards</button>
         {isLoaded === false && <p>Loading...</p>}
         {isLoaded && (
@@ -78,9 +94,6 @@ export default function Home() {
                     name={card.data.name}
                     price={card.data.tcgplayer.prices.holofoil ? card.data.tcgplayer.prices.holofoil.market : card.data.tcgplayer.prices.normal.market}
                   />
-                  {/* <img width={200} src={card.data.images.small} key={index} alt={card.data.name} />
-                  <p>{card.data.name}</p>
-                  <p>{card.data.tcgplayer.prices.holofoil ? card.data.tcgplayer.prices.holofoil.market : card.data.tcgplayer.prices.normal.market}</p> */}
                 </>
               );
             })}
